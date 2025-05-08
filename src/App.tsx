bdf4f022e2e05193ae7b15b8ec47f00eb85b0800
rggarix <gargrajat109@gmail.com>
2025-05-08 16:12:23 +0530

    setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route ,Navigate} from 'react-router-dom';
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/home" /> */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
